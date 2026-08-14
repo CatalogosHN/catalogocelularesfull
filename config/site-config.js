ff5751f7296c1288da2d926c14c69ddbb2c03512
CatalogosHN <sakuratop2025xxx@gmail.com>
2026-08-13 20:41:50 -0600

    Update site-config.js

--- config/site-config.js
+++ config/site-config.js
@@ -1,7 +1,7 @@
 window.CELLZONE_CONFIG = {
   brand: "CellZoneHN",
   domain: "www.celulares.itemshn.com",
-  appsScriptUrl: "PEGAR_AQUI_URL_DE_APPS_SCRIPT",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwYJbh-7Qul_-Dlf-NgucJFalbPs5GGgo6VMlXqsjTKU4kLT9NqCCvolDbLpURPYemenQ/exec",
   whatsapp: "50488597169",
   refreshProductsMs: 30000
 };
