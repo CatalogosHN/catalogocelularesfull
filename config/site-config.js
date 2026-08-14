9939a6a550faa41d99af6efa7ebae92eb6ee8a91
CatalogosHN <sakuratop2025xxx@gmail.com>
2026-08-13 20:40:53 -0600

    Add files via upload

---
+++ config/site-config.js
@@ -0,0 +1,7 @@
+window.CELLZONE_CONFIG = {
+  brand: "CellZoneHN",
+  domain: "www.celulares.itemshn.com",
+  appsScriptUrl: "PEGAR_AQUI_URL_DE_APPS_SCRIPT",
+  whatsapp: "50488597169",
+  refreshProductsMs: 30000
+};
